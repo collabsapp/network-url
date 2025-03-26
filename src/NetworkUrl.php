a36aa162e6a29d6c1e80b57ba358d8bb3e9e446f
Jon Gotlin <jon@jon.se>
2025-03-26 08:00:04 +0100

    Media url (#3)

--- src/NetworkUrl.php
+++ src/NetworkUrl.php
@@ -13,4 +13,14 @@ final class NetworkUrl
             default => throw new InvalidNetworkException($network),
         };
     }
+
+    public static function mediaUrl(string $network, string $username, string $mediaId): string
+    {
+        return match ($network) {
+            'tiktok' => sprintf('https://www.tiktok.com/@%s/video/%s', $username, $mediaId),
+            'instagram' => 'https://www.instagram.com/p/'.$mediaId,
+            'youtube' => 'https://www.youtube.com/watch?v='.$mediaId,
+            default => throw new InvalidNetworkException($network),
+        };
+    }
 }
--- tests/NetworkUrlTest.php
+++ tests/NetworkUrlTest.php
@@ -7,24 +7,45 @@ use PHPUnit\Framework\TestCase;
 
 final class NetworkUrlTest extends TestCase
 {
-    #[DataProvider('dataProvider')]
-    public function test_it_converts_to_url(string $expected, string $network, string $username): void
+    #[DataProvider('profileDataProvider')]
+    public function test_it_converts_to_profile_url(string $expected, string $network, string $username): void
     {
         self::assertSame($expected, NetworkUrl::fullUrl($network, $username));
     }
 
-    public static function dataProvider(): Generator
+    public static function profileDataProvider(): Generator
     {
         yield ['https://www.instagram.com/foobar', 'instagram', 'foobar'];
         yield ['https://www.tiktok.com/@baz', 'tiktok', 'baz'];
         yield ['https://www.youtube.com/foobarbaz', 'youtube', 'foobarbaz'];
     }
 
-    public function test_is_throws_exception_if_invalid_network(): void
+    #[DataProvider('mediaDataProvider')]
+    public function test_it_converts_to_media_url(string $expected, string $network, string $username, string $mediaId): void
+    {
+        self::assertSame($expected, NetworkUrl::mediaUrl($network, $username, $mediaId));
+    }
+
+    public static function mediaDataProvider(): Generator
+    {
+        yield ['https://www.instagram.com/p/aaa', 'instagram', 'foobar', 'aaa'];
+        yield ['https://www.tiktok.com/@baz/video/bbb', 'tiktok', 'baz', 'bbb'];
+        yield ['https://www.youtube.com/watch?v=ccc', 'youtube', 'foobarbaz', 'ccc'];
+    }
+
+    public function test_is_throws_exception_if_invalid_network_for_profile(): void
     {
         self::expectException(InvalidNetworkException::class);
         self::expectExceptionMessage('Invalid network "doge"');
 
         NetworkUrl::fullUrl('doge', 'foo');
+    }
+
+    public function test_is_throws_exception_if_invalid_network_for_media(): void
+    {
+        self::expectException(InvalidNetworkException::class);
+        self::expectExceptionMessage('Invalid network "doge"');
+
+        NetworkUrl::mediaUrl('doge', 'foo', 'bar');
     }
 }
